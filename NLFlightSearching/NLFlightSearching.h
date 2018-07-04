daf5c0021d97d0574c6c943f09286c96aebc3582
Александр Пономарев <gatman55rus@gmail.com>
2018-07-04 21:41:47 +0300

    Initial commit

---
+++ NLFlightSearching/NLFlightSearching.h
@@ -0,0 +1,19 @@
+//
+//  NLFlightSearching.h
+//  NLFlightSearching
+//
+//  Created by Александр Пономарев on 04.07.2018.
+//  Copyright © 2018 Base team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NLFlightSearching.
+FOUNDATION_EXPORT double NLFlightSearchingVersionNumber;
+
+//! Project version string for NLFlightSearching.
+FOUNDATION_EXPORT const unsigned char NLFlightSearchingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NLFlightSearching/PublicHeader.h>
+
+
---
+++ NLFlightSearchingExample/NLFlightSearchingExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NLFlightSearchingExample
+//
+//  Created by Александр Пономарев on 04.07.2018.
+//  Copyright © 2018 Base team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
